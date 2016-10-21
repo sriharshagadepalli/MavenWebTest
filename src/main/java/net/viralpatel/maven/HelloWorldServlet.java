1e645b5daedac163c810a135974930fb1aa29bae
Raj Jha <M1026343@mindtree.com>
2016-10-21 13:27:50 +0530

    Initial Commit

---
+++ src/main/java/net/viralpatel/maven/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package net.viralpatel.maven;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1031422249396784970L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("<center><h1><b> *** Hello World from Servlet *** </b></h1></center>");
+		out.print("<center><h1><b> *** Hello World from Servlet *** </b></h1></center>");
+		out.flush();
+		out.close();
+	}
+}
